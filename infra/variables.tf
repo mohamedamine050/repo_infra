106f05e6c70015655944eeda644b29ab191cc046
mohamed amine abdrrazek <mohamedamine.abdrrazek@supcom.tn>
2026-05-15 22:23:23 +0100

    Add AWS RDS configuration variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,46 @@ variable "create_temp_bucket" {
   description = "Whether to create an optional temp bucket for Glue staging."
   default     = false
 }
+
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "db_identifier" {
+  type        = string
+  description = "RDS identifier"
+  default     = "postgres-rds"
+}
+
+variable "db_name" {
+  type        = string
+  description = "Database name"
+  default     = "mydatabase"
+}
+
+variable "db_username" {
+  type        = string
+  description = "Database username"
+  default     = "admin"
+}
+
+variable "db_password" {
+  type        = string
+  description = "Database password"
+  sensitive   = true
+}
+
+variable "instance_class" {
+  type        = string
+  description = "RDS instance class"
+  default     = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  type        = number
+  description = "Storage size in GB"
+  default     = 20
+}
